a468b630df17bfe774ebee599aefb5bb9cb5438a
RayzerGamer <louis.karamucki@outlook.fr>
2022-06-13 11:18:29 +0200

    Msg discord logs ready

--- events/ready/check_voc_bdd.js
+++ events/ready/check_voc_bdd.js
@@ -11,7 +11,14 @@ module.exports = {
                         const name_chan = channel.name
                         channel.delete()
                             .then(client.database.query('DELETE FROM ' + client.config.MySQL.tables.VocalChannel + ' WHERE id_channel = ?', [voc.id_channel]), 
-                            console.log(`${client.Func.LogDate()}Le channel ${name_chan} a bien été supprimé`.cyan))
+                            console.log(`${client.Func.LogDate()}Le channel ${name_chan} a été supprimé`.cyan))
+                            const channel = client.channels.cache.find(x => x.id == client.config.Discord.IDSalonServer.Logs)
+                            const Embed = new MessageEmbed()
+                                .setColor('#00FFFF')
+                                .setTitle('Vocals Manager')
+                                .setDescription(`Le salon vocal de ${client.users.cache.get(voc.id_author).username} a été supprimé`)
+                                .setTimestamp()
+                            channel.send({embeds: [Embed]})
                     }
                 }
             })
