8ebf51035ad1e34e7fd612af9fece661be8bce96
RayzerDev <80360820+RayzerGamer@users.noreply.github.com>
2022-09-08 20:43:01 +0200

    RESET → Menu des grades

--- selectmenus/roles_menus/grades.js
+++ selectmenus/roles_menus/grades.js
@@ -1,22 +1,13 @@
-
 module.exports = {
 	name: 'roles_grades',
 
 	async execute(interaction, client) {
-        for(role of interaction.values) {		    
+        for(role of interaction.values) {
             if (interaction.member.roles.resolve(role)){
                 interaction.member.roles.remove(role);
             }
             else{
-		if (interaction.member.id != "690177619228229688" && role != "835092000696041503"){
-                    interaction.member.roles.add(role);
-		}
-		else{
-		    interaction.reply({
-		        content:"Cheh tu peux pas être développeur KIKI",
-		        ephemeral:true
-		    })
-		}
+                interaction.member.roles.add(role);
             }
         }
 
